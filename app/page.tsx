0739d585918f97bb307bdf5cc652c73a6ea13a75
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-28 19:05:21 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { PlusIcon } from 'lucide-react';
+import { Button, type ButtonProps } from '@/components/demo-components-buttons-button';
+
+interface ButtonDemoProps {
+  variant: ButtonProps['variant'];
+  size: ButtonProps['size'];
+}
+
+export default function ButtonDemo({ variant, size }: ButtonDemoProps) {
+  return (
+    <Button variant={variant} size={size}>
+      {size === 'icon' ? <PlusIcon /> : 'Click me'}
+    </Button>
+  );
+}
